0ffa98c3c28ad0465a0d4b9057cefd6fd563f955
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-07-12 17:46:28 +0600

    Start

---
+++ SpaceGame/GameScene.swift
@@ -0,0 +1,52 @@
+//
+//  GameScene.swift
+//  SpaceGame
+//
+//  Created by p1us on 26.08.15.
+//  Copyright (c) 2015 Ivan Akulov. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene  {
+  
+  // создаем свойства
+  var spaceShip: SKSpriteNode!
+  var background: SKSpriteNode!
+  
+  override func didMoveToView(view: SKView) {
+    
+    //создаем фон
+    let width = UIScreen.mainScreen().bounds.size.width  //frame.size.width
+    let height = UIScreen.mainScreen().bounds.size.height
+    
+    background = SKSpriteNode(imageNamed: "background")
+    background.position = CGPoint(x: width / 2, y: height / 2)
+    background.size = CGSize(width: width, height: height)
+    addChild(background)
+    
+    //Создаем космический корабль и определяем начальную позицию на экране
+    spaceShip = SKSpriteNode(imageNamed: "spaceShip")
+    spaceShip.position = CGPoint(x: 200, y: 200)
+    
+    addChild(spaceShip)
+    
+    background.zPosition = 0
+    spaceShip.zPosition = 1
+  }
+  
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+    if let touch = touches.first {
+      
+      // определяем точку прикосновения с экраном
+      let touchLocation = touch.locationInNode(self)
+      
+      let moveAction = SKAction.moveTo(touchLocation, duration: 1)
+      moveAction.timingMode = SKActionTimingMode.EaseInEaseOut
+      
+      spaceShip.runAction(moveAction)
+      
+    }
+  }
+}
